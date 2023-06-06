f45cadc3e7ff763fbf8b863c9408e3302f82d299
Candido Sales Gomes <candidosg@gmail.com>
2023-06-06 11:59:04 -0400

    improve animation, sitemap, compress, fix images

---
+++ src/data/portfolio.ts
@@ -0,0 +1,238 @@
+export const portfolio = [
+  {
+    imageUrl: "/portfolio/material-time-picker.png",
+    imageAlt: "Material Time Picker",
+    title: "Material Time Picker",
+    description: "A simple time picker component using Angular Material.",
+    url: "https://github.com/candidosales/material-time-picker",
+    techs: [
+      {
+        icon: "angular",
+        name: "Angular",
+      },
+      {
+        icon: "npm",
+        name: "NPM",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/svelte-google-translate.jpg",
+    imageAlt: "Svelte Google Translate",
+    title: "Svelte Google Translate",
+    description: "Google Translate component for Svelte.",
+    url: "https://github.com/candidosales/svelte-google-translate",
+    techs: [
+      {
+        icon: "svelte",
+        name: "Svelte",
+      },
+      {
+        icon: "google-translate",
+        name: "Google Translate",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/format-code.png",
+    imageAlt: "Format Code",
+    title: "Format Code",
+    description:
+      "A Figma plugin designed to allow you to format and syntax highlight code",
+    url: "https://www.figma.com/community/plugin/891351519501239645/Format-Code",
+    techs: [
+      {
+        icon: "figma",
+        name: "Figma",
+      },
+      {
+        icon: "typescript",
+        name: "Typescript",
+      },
+      {
+        icon: "sass",
+        name: "SASS",
+      },
+      {
+        icon: "webpack",
+        name: "Webpack",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/landing-page-book-2v.jpg",
+    imageAlt: "Landing Page Book 2v",
+    title: "Landing Page Book 2v",
+    description:
+      "Simple landing page to sell your book - Astro + Svelte + Tailwind",
+    url: "https://github.com/candidosales/landing-page-book-astro-svelte-tailwind",
+    techs: [
+      {
+        icon: "astro",
+        name: "Astro",
+      },
+      {
+        icon: "svelte",
+        name: "Svelte",
+      },
+      {
+        icon: "tailwind",
+        name: "Tailwind",
+      },
+      {
+        icon: "partytown",
+        name: "Partytown",
+      },
+      {
+        icon: "typescript",
+        name: "Typescript",
+      },
+      {
+        icon: "vercel",
+        name: "Vercel",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/figma-plugin-svelte-vite.png",
+    imageAlt: "Figma Plugin + Svelte + Vite + Typescript    ",
+    title: "Figma Plugin + Svelte + Vite + Typescript",
+    description:
+      "A boilerplate for creating Figma plugins with Svelte, Vite and Typescript",
+    url: "https://github.com/candidosales/figma-plugin-svelte-vite",
+    techs: [
+      {
+        icon: "figma",
+        name: "Figma",
+      },
+      {
+        icon: "svelte",
+        name: "Svelte",
+      },
+      {
+        icon: "vite",
+        name: "Vite",
+      },
+      {
+        icon: "typescript",
+        name: "Typescript",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/dependency-report-optimized.png",
+    imageAlt: "Dependencies report",
+    title: "Dependencies report",
+    description:
+      "Dependencies report aims to help analyze the consistency of the dependencies in your company's frontend projects.",
+    url: "https://github.com/candidosales/dependency-report",
+    techs: [
+      {
+        icon: "angular",
+        name: "Angular",
+      },
+      {
+        icon: "go",
+        name: "Go",
+      },
+      {
+        icon: "firebase",
+        name: "Firebase",
+      },
+      {
+        icon: "docker",
+        name: "Docker",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/cloud-function-go.png",
+    imageAlt: "API in GO to create Cloud Function",
+    title: "API in GO to create Cloud Function",
+    description: "🐹 An API for deploying Google Cloud Functions in Go.",
+    url: "https://github.com/candidosales/cloud-function-go",
+    techs: [
+      {
+        icon: "go",
+        name: "Go",
+      },
+      {
+        icon: "google-cloud",
+        name: "Google Cloud",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/landing-page-book-1v.png",
+    imageAlt: "Landing Page Book 1v",
+    title: "Landing Page Book 1v",
+    description:
+      "Landing page for advertising and sales of books to independent authors",
+    url: "https://github.com/candidosales/landing-page-book",
+    techs: [
+      {
+        icon: "sass",
+        name: "SASS",
+      },
+      {
+        icon: "bootstrap",
+        name: "Bootstrap",
+      },
+      {
+        icon: "webpack",
+        name: "Webpack",
+      },
+      {
+        icon: "netlify",
+        name: "Netlify",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/personal-blog.png",
+    imageAlt: "Personal Portfolio",
+    title: "Personal Portfolio",
+    description: "Landing page for your personal portfolio",
+    url: "https://github.com/candidosales/personal-blog",
+    techs: [
+      {
+        icon: "nextjs",
+        name: "NextJS",
+      },
+      {
+        icon: "react",
+        name: "React",
+      },
+      {
+        icon: "chakra-ui",
+        name: "Chakra UI",
+      },
+      {
+        icon: "sass",
+        name: "SASS",
+      },
+      {
+        icon: "vercel",
+        name: "Vercel",
+      },
+      {
+        icon: "pwa",
+        name: "PWA",
+      },
+    ],
+  },
+  {
+    imageUrl: "/portfolio/empresas-angular.png",
+    imageAlt: "Empresas que usam Angular no Brasil",
+    title: "Empresas que usam Angular no Brasil",
+    description:
+      "Repository of companies in 🇧🇷 that use Angular in their projects",
+    url: "https://github.com/candidosales/empresas-que-usam-angular-no-brasil",
+    techs: [
+      {
+        icon: "github",
+        name: "Github",
+      },
+    ],
+  },
+];
