19812b6b2f18841263f13372039424a4c01a227b
Candido Sales Gomes <candidosg@gmail.com>
2023-06-13 10:39:57 -0400

    added quote and extras

---
+++ src/data/extras.ts
@@ -0,0 +1,100 @@
+export const extras = [
+  // {
+  //   imageUrl: "/were-not-really-strangers.png",
+  //   imageAlt: "We're Not Really Strangers",
+  //   title: "We're Not Really Strangers",
+  //   description:
+  //     "I ❤️ this game and wanted to create a possibility to play with my friends online and get to know each other even from a distance. (COVID tough times 😞)",
+  //   url: "https://wnrs.app/",
+  //   techs: [
+  //     {
+  //       icon: "nextjs",
+  //       name: "NextJS",
+  //     },
+  //     {
+  //       icon: "react",
+  //       name: "React",
+  //     },
+  //     {
+  //       icon: "chakra-ui",
+  //       name: "Chakra UI",
+  //     },
+  //     {
+  //       icon: "sass",
+  //       name: "SASS",
+  //     },
+  //     {
+  //       icon: "vercel",
+  //       name: "Vercel",
+  //     },
+  //     {
+  //       icon: "pwa",
+  //       name: "PWA",
+  //     },
+  //   ],
+  // },
+  {
+    imageUrl: "/mock-2v.png",
+    imageAlt: "Receituário Médico SUS",
+    title: "Receituário Médico SUS",
+    description:
+      "It helps doctors from the Brazilian public health system make medical prescriptions for free",
+    url: "https://receituariomedico.app/",
+    techs: [
+      {
+        icon: "nextjs",
+        name: "NextJS",
+      },
+      {
+        icon: "chakra-ui",
+        name: "Chakra UI",
+      },
+      {
+        icon: "supabase",
+        name: "Supabase",
+      },
+      {
+        icon: "upstash",
+        name: "Upstash",
+      },
+      {
+        icon: "vercel",
+        name: "Vercel",
+      },
+      {
+        icon: "pwa",
+        name: "PWA",
+      },
+    ],
+  },
+  {
+    imageUrl: "/mock-2v.png",
+    imageAlt: "Virada Cultural",
+    title: "My Cultural Agenda",
+    description:
+      "PWA website that helps you organize your agenda at large events",
+    url: "https://carnaval-sp-ec736.web.app/",
+    techs: [
+      {
+        icon: "angular",
+        name: "Angular",
+      },
+      {
+        icon: "firebase",
+        name: "Firebase",
+      },
+      {
+        icon: "ruby",
+        name: "Ruby",
+      },
+      {
+        icon: "rails",
+        name: "Rails",
+      },
+      {
+        icon: "pwa",
+        name: "PWA",
+      },
+    ],
+  },
+];
