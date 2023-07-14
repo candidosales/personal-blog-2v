150f838eecf950288625a54f2d96afd14d656f1b
Candido Sales Gomes <candidosg@gmail.com>
2023-07-14 09:15:40 -0400

    improve footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import TextLink from "./TextLink.svelte";
+  import Github from "./icons/Github.svelte";
+  import Linkedin from "./icons/Linkedin.svelte";
+  import Mail from "./icons/Mail.svelte";
+  import Twitter from "./icons/Twitter.svelte";
+
+  const links = [
+    {
+      href: "/contact",
+      label: "E-mail",
+      targetBlank: true,
+      icon: Mail,
+    },
+    {
+      href: "https://twitter.com/candidosales",
+      label: "Twitter",
+      targetBlank: true,
+      icon: Twitter,
+    },
+    {
+      href: "https://github.com/candidosales",
+      label: "Github",
+      targetBlank: true,
+      icon: Github,
+    },
+    {
+      href: "https://linkedin.com/in/candidosales",
+      label: "Linkedin",
+      targetBlank: true,
+      icon: Linkedin,
+    },
+    {
+      href: "https://adplist.org/mentors/candido-sales-gomes",
+      label: "Mentoring",
+      targetBlank: true,
+    },
+  ];
+</script>
+
+<footer class="w-full flex flex-row justify-center items-center lowercase">
+  {#each links as l}
+    <div class="m-4">
+      <TextLink href={l.href} title={l.label} label={l.label}
+        ><div class="w-4 h-4 mr-2 flex items-center align-middle text-blue-600">
+          <svelte:component this={l.icon} />
+        </div></TextLink
+      >
+    </div>
+  {/each}
+</footer>
--- src/components/TextLink.svelte
+++ src/components/TextLink.svelte
@@ -1,14 +1,13 @@
 <script>
-  import Link from "./icons/Link.svelte";
   export let label = "";
   export let title = "";
   export let href = "";
 </script>
 
 <a
-  class="underline underline-offset-4 text-blue-600"
+  class="underline underline-offset-4 text-blue-600 after:content-['_↗'] flex flex-row align-middle items-center"
   font-medium
   {href}
   target="_blank"
-  {title}>{label}</a
+  {title}><slot /> {label}</a
 >
