6c5d3184cb417ef32b7861ebaf65294eddbff796
Candido Sales Gomes <candidosg@gmail.com>
2023-07-16 20:42:14 -0400

    blog section

---
+++ src/data/posts.ts
@@ -0,0 +1,64 @@
+export const posts = [
+  {
+    imageUrl: "/articles/post-organize-figma.webp",
+    imageAlt: "How to organize Figma files for developers",
+    title: "How to organize Figma files for developers",
+    description:
+      "I going to share a simple tip that I find recently that helps the productivity of our team",
+    href: "https://dev.to/candidosales/how-to-organize-figma-files-for-developers-2lcj",
+    publishDate: "2022-11-21",
+  },
+  {
+    imageUrl: "/articles/post-strapi-digital-ocean.webp",
+    imageAlt: "Deploying Strapi 4 to Digital Ocean App",
+    title: "Deploying Strapi 4 to Digital Ocean App",
+    description:
+      "Step by step on how to deploying Strapi 4 to Digital Ocean App",
+    href: "https://dev.to/candidosales/deploying-strapi-4-to-digital-ocean-app-1d8d",
+    publishDate: "2022-08-15",
+  },
+  {
+    imageUrl: "/articles/post-strapi-railway.webp",
+    imageAlt: "Deploying Strapi 4 to Railway",
+    title: "Deploying Strapi 4 to Railway",
+    description: "Step by step on how to deploying Strapi 4 to Railway",
+    href: "https://dev.to/candidosales/deploying-strapi-4-to-railway-589b",
+    publishDate: "2022-08-03",
+  },
+  {
+    imageUrl: "/articles/post-medicina.jpeg",
+    imageAlt: "O que a medicina pode aprender com a programação?",
+    title: "O que a medicina pode aprender com a programação?",
+    description:
+      "Esse texto são apenas reflexões que foram geradas durante diálogos entre um engenheiro de software e uma médica.",
+    href: "https://medium.com/@candidosales/o-que-a-medicina-pode-aprender-com-a-programa%C3%A7%C3%A3o-4ca71bd94fc4",
+    publishDate: "2022-05-01",
+  },
+  {
+    imageUrl: "/articles/post-1.png",
+    imageAlt: "How I got a job offer in Canada",
+    title: "How I got a job offer in Canada",
+    description:
+      "In this article, I’ll present the process I did to get the job offer through VanHack",
+    href: "https://blog.usejournal.com/how-did-i-get-a-job-offer-in-canada-3351937653ad",
+    publishDate: "2019-08-26",
+  },
+  {
+    imageUrl: "/articles/post-2.png",
+    imageAlt: "Como consegui uma oferta de emprego no Canadá",
+    title: "Como consegui uma oferta de emprego no Canadá",
+    description:
+      "Vou apresentar o processo que fiz para conseguir a oferta de emprego através da VanHack",
+    href: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
+    publishDate: "2019-08-18",
+  },
+  {
+    imageUrl: "/articles/post-3.png",
+    imageAlt: "Como fiz o intercâmbio para o Canadá",
+    title: "Como fiz o intercâmbio para o Canadá",
+    description:
+      "Vou contar a minha experiência de como me mudei para o Canadá",
+    href: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
+    publishDate: "2019-08-18",
+  },
+];
