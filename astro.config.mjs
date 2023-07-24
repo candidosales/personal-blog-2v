13db6d6d619fc492fc8920d32d55cb6489da5449
Candido Sales Gomes <candidosg@gmail.com>
2023-07-24 00:43:56 -0400

    small improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,12 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.candidosales.me/",
+  redirects: {
+    "/twitter": "https://twitter.com/candidosales",
+    "/linkedin": "https://www.linkedin.com/in/candidosales/",
+    "/mentorship": "https://adplist.org/mentors/candido-sales-gomes",
+    "/github": "https://github.com/candidosales",
+  },
   integrations: [
     tailwind(),
     image({
