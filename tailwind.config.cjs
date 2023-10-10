7ab92526d412eb1d4cff350cca738895a6f43fec
Candido Sales Gomes <candidosg@gmail.com>
2023-10-10 18:58:51 -0300

    improved typo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        inclusive: ["Inclusive Sans", "Inter", ...defaultTheme.fontFamily.sans],
-        calligraffitti: [
-          "'Calligraffitti', cursive",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        inclusive: ['Inclusive Sans', 'Inter', ...defaultTheme.fontFamily.sans],
+        newsreader: ["'Newsreader', serif", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('flowbite/plugin')],
 };
