4e719f22d7cdc21a373bbf21b85c8700e1d05283
Candido Sales Gomes <candidosg@gmail.com>
2024-02-05 12:13:23 -0500

    improved SEO and menu section in mobile

--- src/components/MenuMobile.svelte
+++ src/components/MenuMobile.svelte
@@ -23,7 +23,7 @@
 <div class="fixed w-full bottom-6 right-8 z-10 md:hidden">
   <div class="-mr-2 flex justify-end md:hidden">
     <Button
-      class="bg-blue-100/80 backdrop-blur-md rounded-full border border-solid border-blue-200 px-4 py-3 inline-flex items-center justify-center font-medium text-blue-800 hover:text-blue-300  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500 drop-shadow-xl font-inclusive"
+      class="bg-blue-700 text-white backdrop-blur-md rounded-full px-4 py-3 inline-flex items-center justify-center font-medium focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500 drop-shadow-xl font-inclusive"
       on:click={() => (drawerHidden = false)}
     >
       Menu</Button
@@ -42,7 +42,7 @@
   <div class="flex items-center">
     <h5
       id="drawer-navigation-label-3"
-      class="text-base font-medium text-blue-800 font-inclusive"
+      class="text-2xl font-medium text-blue-800 font-inclusive"
     >
       Menu
     </h5>
@@ -56,7 +56,7 @@
       {#each menuItems as m}
         <SidebarItem
           href={m.href}
-          aClass="flex items-center p-2 text-base font-normal text-blue-800 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-blue-700"
+          aClass="flex items-center p-2 font-normal text-blue-800 rounded-lg dark:text-white hover:bg-blue-100 dark:hover:bg-blue-700"
         >
           <svelte:fragment slot="icon">
             <span class="text-blue-800">
@@ -64,7 +64,7 @@
             </span>
           </svelte:fragment>
           <svelte:fragment slot="subtext">
-            <span class="text-blue-800 font-inclusive">
+            <span class="text-blue-800 font-inclusive text-lg">
               {m.label}
             </span>
           </svelte:fragment>
