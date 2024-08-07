e1a988fd9ebd9f5f301dc4ef78a1b240b78fc7fe
Candido Sales Gomes <candidosg@gmail.com>
2024-08-07 13:10:16 -0400

    create the components

---
+++ src/components/ButtonCup.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import CheckmarkSolid from './icons/CheckmarkSolid.svelte';
+
+  export let label = 'button';
+  export let info = 'info';
+  export let color = 'blue';
+  export let selected = false;
+
+  const dispatch = createEventDispatcher();
+  const click = (): void => {
+    dispatch('click');
+  };
+</script>
+
+<div class="flex flex-col text-center mb-16">
+  <button
+    on:click={click}
+    class={`${selected ? `bg-${color}-500` : `bg-${color}-400`} hover:bg-${color}-700 text-center text-white font-bold py-4 px-6 rounded-full mb-2 transition-all`}
+  >
+    {#if selected}
+      <CheckmarkSolid class="mr-4 size-6 absolute" />
+    {/if}
+    {label}
+  </button>
+  <p class="text-sm">{info}</p>
+</div>
+
+<style>
+  .bg-green-400 {
+    background-color: #4ade80;
+  }
+  .bg-yellow-400 {
+    background-color: #facc15;
+  }
+  .bg-red-400 {
+    background-color: #f87171;
+  }
+</style>
---
+++ src/components/ButtonListCup.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import ButtonCup from './ButtonCup.svelte';
+
+  const buttons = [
+    {
+      label: 'Estou ok',
+      color: 'green',
+      info: 'Estou confortável com meu entendimento e o ritmo da apresentação',
+      value: 'ok',
+      selected: false,
+    },
+    {
+      label: 'Não tenho certeza',
+      color: 'yellow',
+      info: 'Estou tentando acompanhar, mas você poderia desacelerar um pouco ou revisar o conceito em que estamos',
+      value: 'uncertain',
+      selected: false,
+    },
+    {
+      label: 'Pare',
+      color: 'red',
+      info: 'Não estou entendendo e tenho uma pergunta',
+      value: 'stop',
+      selected: false,
+    },
+  ];
+
+  function vote(index: number) {
+    if (buttons[index].selected == false) {
+      console.log('vote', index, buttons[index]);
+
+      buttons.map((b) => {
+        b.selected = false;
+      });
+
+      buttons[index].selected = true;
+
+      sendVote(buttons[index].value);
+    }
+  }
+
+  function sendVote(value: string) {}
+</script>
+
+<div class="cup-buttons h-screen flex flex-col justify-center">
+  {#each buttons as { label, color, info, selected }, index}
+    <ButtonCup {label} {color} {info} {selected} on:click={() => vote(index)} />
+  {/each}
+</div>
