0563a3f40a13b88737c8c48c42064deaccde5672
Candido Sales Gomes <candidosg@gmail.com>
2024-08-20 18:36:49 -0400

    fix spaces and center image

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,9 +1,9 @@
 export const posts = [
   {
-	image: {
-		url: "/articles/post-migrate-render-flyio.webp",
-    	alt: "Migrate Strapi from Render to Fly.io",
-	},
+    image: {
+      url: "/articles/post-migrate-render-flyio.webp",
+      alt: "Migrate Strapi from Render to Fly.io",
+    },
     title: "Migrate Strapi from Render to Fly.io",
     description:
       "I was having cost issues with the Render server, and I can't reduce the server for small machines. Most of my users are from Brazil, so I moved my Strapi to Fly.io, which enables reduced costs.",
@@ -11,10 +11,10 @@ export const posts = [
     publishDate: "2024-08-01",
   },
   {
-	image: {
-		url: "/articles/post-organize-figma.webp",
-    	alt: "How to organize Figma files for developers",
-	},
+    image: {
+      url: "/articles/post-organize-figma.webp",
+      alt: "How to organize Figma files for developers",
+    },
     title: "How to organize Figma files for developers",
     description:
       "I going to share a simple tip that I find recently that helps the productivity of our team",
@@ -22,10 +22,10 @@ export const posts = [
     publishDate: "2022-11-21",
   },
   {
-	image: {
-		url: "/articles/post-strapi-digital-ocean.webp",
-    	alt: "Deploying Strapi 4 to Digital Ocean App",
-	},
+    image: {
+      url: "/articles/post-strapi-digital-ocean.webp",
+      alt: "Deploying Strapi 4 to Digital Ocean App",
+    },
     title: "Deploying Strapi 4 to Digital Ocean App",
     description:
       "Step by step on how to deploying Strapi 4 to Digital Ocean App",
@@ -33,20 +33,20 @@ export const posts = [
     publishDate: "2022-08-15",
   },
   {
-	image: {
-		url: "/articles/post-strapi-railway.webp",
-    	alt: "Deploying Strapi 4 to Railway",
-	},
+    image: {
+      url: "/articles/post-strapi-railway.webp",
+      alt: "Deploying Strapi 4 to Railway",
+    },
     title: "Deploying Strapi 4 to Railway",
     description: "Step by step on how to deploying Strapi 4 to Railway",
     href: "https://dev.to/candidosales/deploying-strapi-4-to-railway-589b",
     publishDate: "2022-08-03",
   },
   {
-	image: {
-		url:  "/articles/post-medicina.jpeg",
-		alt: "O que a medicina pode aprender com a programação?",
-	},
+    image: {
+      url: "/articles/post-medicina.jpeg",
+      alt: "O que a medicina pode aprender com a programação?",
+    },
     title: "O que a medicina pode aprender com a programação?",
     description:
       "Esse texto são apenas reflexões que foram geradas durante diálogos entre um engenheiro de software e uma médica.",
@@ -54,10 +54,10 @@ export const posts = [
     publishDate: "2022-05-01",
   },
   {
-	image: {
-		url: "/articles/post-1.png",
-		alt: "How I got a job offer in Canada",
-	},
+    image: {
+      url: "/articles/post-1.png",
+      alt: "How I got a job offer in Canada",
+    },
     title: "How I got a job offer in Canada",
     description:
       "In this article, I’ll present the process I did to get the job offer through VanHack",
@@ -65,10 +65,10 @@ export const posts = [
     publishDate: "2019-08-26",
   },
   {
-	image: {
-		url: "/articles/post-2.png",
-    	alt: "Como consegui uma oferta de emprego no Canadá",
-	},
+    image: {
+      url: "/articles/post-2.png",
+      alt: "Como consegui uma oferta de emprego no Canadá",
+    },
     title: "Como consegui uma oferta de emprego no Canadá",
     description:
       "Vou apresentar o processo que fiz para conseguir a oferta de emprego através da VanHack",
@@ -76,10 +76,10 @@ export const posts = [
     publishDate: "2019-08-18",
   },
   {
-	image: {
-		url: "/articles/post-3.png",
-    	alt: "Como fiz o intercâmbio para o Canadá",
-	},
+    image: {
+      url: "/articles/post-3.png",
+      alt: "Como fiz o intercâmbio para o Canadá",
+    },
     title: "Como fiz o intercâmbio para o Canadá",
     description:
       "Vou contar a minha experiência de como me mudei para o Canadá",
--- src/data/presentations.ts
+++ src/data/presentations.ts
@@ -1,100 +1,100 @@
 export const presentations = [
-	{
-	  image: {
-		url: "/presentations/turismo-piaui-ideia-implementacao.png",
-		alt: "Turismo Piauí: da Ideia a Implementação"
-	  },
-	  title: "Turismo Piauí: da Ideia a Implementação",
-	  description:
-		"Descubra o passo a passo da criação do Turismo Piauí, desde o planejamento até a implementação. Abordaremos as decisões tecnológicas, como o uso de Svelte, Strapi e Meilisearch, além de explorar a arquitetura e curiosidades do processo. Uma visão prática e direta para quem deseja entender os bastidores de um projeto digital",
-	  href: "https://docs.google.com/presentation/d/1mcDK6eYoHp5HQKdI1ZKyEvBvwl3C0no5Q51sSxfD_dA/edit?usp=sharing",
-	  publishDate: "2024-08-22",
-	  place: {
-		name: "Teresina Info"
-	  }
-	},
-	{
-		image: {
-			url: "/presentations/metodos-ageis.png",
-			alt: "Métodos Ágeis e Scrum",
-		  },
-		title: "Métodos Ágeis e Scrum",
-		description:
-		  "Apresentação realizada para o time do governo do estado sobre métodos ágeis e Scrum",
-		href: "https://docs.google.com/presentation/d/1kHT9DDp5IFxB19JkszEoRc746LgWwUwbcpzwcSydRKc/edit?usp=sharing",
-		publishDate: "2023-08-23",
-		place: {
-			name: "Governo do Piauí"
-		  }
-	  },
-	  {
-		image: {
-			url: "/presentations/simbiose-tecnologia.png",
-			alt: "Simbiose da Administração com a Tecnologia",
-		  },
-		title: "Simbiose da Administração com a Tecnologia",
-		description:
-		  "Contar um pouco de como podemos relacionar a tecnologia na administração, como podemos tirar bons frutos dessa relação e um pouco do panorama da tecnologia",
-		href: "https://speakerdeck.com/candidosales/como-tornar-seguro-um-aplicativo-mobile-payment-sem-penalizar-a-experiencia-do-usuario",
-		publishDate: "2021-11-01",
-		place: {
-			name: "UESPI"
-		  }
-	  },
-	  {
-		image: {
-			url: "/presentations/vc+-app-seguro.jpg",
-			alt: "Como tornar seguro um aplicativo mobile payment sem penalizar a experiência do usuário",
-		  },
-		title: "Como tornar seguro um aplicativo mobile payment sem penalizar a experiência do usuário",
-		description:
-		  "",
-		href: "https://speakerdeck.com/candidosales/como-tornar-seguro-um-aplicativo-mobile-payment-sem-penalizar-a-experiencia-do-usuario",
-		publishDate: "2018-07-19",
-		place: {
-			name: "The Developers Conference"
-		  }
-	  },
-	{
-		image: {
-			url: "/presentations/case-study-secured-financial-mobile.jpg",
-	  		alt: "Case Study: How we've secured a financial mobile application without penalizing the user experience",
-		  },
-	  title: "Case Study: How we've secured a financial mobile application without penalizing the user experience",
-	  description:
-		"",
-	  href: "https://speakerdeck.com/candidosales/case-study-how-weve-secured-a-financial-mobile-application-without-penalizing-the-user-experience",
-	  publishDate: "2017-10-02",
-	  place: {
-		name: "ISC2 Latam - Security Congress"
-	  }
-	},
-	{
-		image: {
-			url: "/presentations/como-browsers-funcionam.jpg",
-			alt: "Como os browsers funcionam - Parte 1",
-		  },
-		title: "Como os browsers funcionam - Parte 1",
-		description: "Explicar como os mecanismos de renderização (Gecko e Webkit) interpretam e constroem a árvore DOM",
-		href: "https://speakerdeck.com/candidosales/como-os-browsers-funcionam-parte-1",
-		publishDate: "2014-02-09",
-		place: {
-			name: "FronThend"
-		  }
-	  },
-	  {
-		image: {
-			url: "/presentations/frontend-modular-escalavel.jpg",
-			alt: "Por que criar um front-end modular e escalável",
-		  },
-		title: "Por que criar um front-end modular e escalável",
-		description:
-		  "Refletir a importância de perceber o quanto pode crescer sua aplicação e com isso você cria uma arquitetura modular e escalável para que outras equipes, ou até você mesmo, possam trabalhar de maneira organizada e sustentável.",
-		href: "https://speakerdeck.com/candidosales/por-que-criar-um-front-end-modular-e-escalavel",
-		publishDate: "2014-02-09",
-		place: {
-			name: "FronThend"
-		  }
-	  },
-  ];
-  
+  {
+    image: {
+      url: "/presentations/turismo-piaui-ideia-implementacao.png",
+      alt: "Turismo Piauí: da Ideia a Implementação",
+    },
+    title: "Turismo Piauí: da Ideia a Implementação",
+    description:
+      "Descubra o passo a passo da criação do Turismo Piauí, desde o planejamento até a implementação. Abordaremos as decisões tecnológicas, como o uso de Svelte, Strapi e Meilisearch, além de explorar a arquitetura e curiosidades do processo. Uma visão prática e direta para quem deseja entender os bastidores de um projeto digital",
+    href: "https://docs.google.com/presentation/d/1mcDK6eYoHp5HQKdI1ZKyEvBvwl3C0no5Q51sSxfD_dA/edit?usp=sharing",
+    publishDate: "2024-08-22",
+    place: {
+      name: "Teresina Info",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/metodos-ageis.png",
+      alt: "Métodos Ágeis e Scrum",
+    },
+    title: "Métodos Ágeis e Scrum",
+    description:
+      "Apresentação realizada para o time do governo do estado sobre métodos ágeis e Scrum",
+    href: "https://docs.google.com/presentation/d/1kHT9DDp5IFxB19JkszEoRc746LgWwUwbcpzwcSydRKc/edit?usp=sharing",
+    publishDate: "2023-08-23",
+    place: {
+      name: "Governo do Piauí",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/simbiose-tecnologia.png",
+      alt: "Simbiose da Administração com a Tecnologia",
+    },
+    title: "Simbiose da Administração com a Tecnologia",
+    description:
+      "Contar um pouco de como podemos relacionar a tecnologia na administração, como podemos tirar bons frutos dessa relação e um pouco do panorama da tecnologia",
+    href: "https://speakerdeck.com/candidosales/como-tornar-seguro-um-aplicativo-mobile-payment-sem-penalizar-a-experiencia-do-usuario",
+    publishDate: "2021-11-01",
+    place: {
+      name: "UESPI",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/vc+-app-seguro.jpg",
+      alt: "Como tornar seguro um aplicativo mobile payment sem penalizar a experiência do usuário",
+    },
+    title:
+      "Como tornar seguro um aplicativo mobile payment sem penalizar a experiência do usuário",
+    description: "",
+    href: "https://speakerdeck.com/candidosales/como-tornar-seguro-um-aplicativo-mobile-payment-sem-penalizar-a-experiencia-do-usuario",
+    publishDate: "2018-07-19",
+    place: {
+      name: "The Developers Conference",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/case-study-secured-financial-mobile.jpg",
+      alt: "Case Study: How we've secured a financial mobile application without penalizing the user experience",
+    },
+    title:
+      "Case Study: How we've secured a financial mobile application without penalizing the user experience",
+    description: "",
+    href: "https://speakerdeck.com/candidosales/case-study-how-weve-secured-a-financial-mobile-application-without-penalizing-the-user-experience",
+    publishDate: "2017-10-02",
+    place: {
+      name: "ISC2 Latam - Security Congress",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/como-browsers-funcionam.jpg",
+      alt: "Como os browsers funcionam - Parte 1",
+    },
+    title: "Como os browsers funcionam - Parte 1",
+    description:
+      "Explicar como os mecanismos de renderização (Gecko e Webkit) interpretam e constroem a árvore DOM",
+    href: "https://speakerdeck.com/candidosales/como-os-browsers-funcionam-parte-1",
+    publishDate: "2014-02-09",
+    place: {
+      name: "FronThend",
+    },
+  },
+  {
+    image: {
+      url: "/presentations/frontend-modular-escalavel.jpg",
+      alt: "Por que criar um front-end modular e escalável",
+    },
+    title: "Por que criar um front-end modular e escalável",
+    description:
+      "Refletir a importância de perceber o quanto pode crescer sua aplicação e com isso você cria uma arquitetura modular e escalável para que outras equipes, ou até você mesmo, possam trabalhar de maneira organizada e sustentável.",
+    href: "https://speakerdeck.com/candidosales/por-que-criar-um-front-end-modular-e-escalavel",
+    publishDate: "2014-02-09",
+    place: {
+      name: "FronThend",
+    },
+  },
+];
